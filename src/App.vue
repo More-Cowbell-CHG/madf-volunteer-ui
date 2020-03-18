effd92a3595f2ec8aa40555ffcd82e46a282687a
Fernando <fernando.fernandez@chghealthcare.com>
2020-03-18 10:50:47 -0600

    feature(site nav): HTML structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-container>
+          <b-navbar-brand href="#">Site Nav</b-navbar-brand>
+  
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  
+          <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav>
+                <b-nav-item href="#">Home</b-nav-item>
+                <b-nav-item href="#">About</b-nav-item>
+                <b-nav-item href="#">Contact</b-nav-item>
+              </b-navbar-nav>
+  
+              <b-navbar-nav class="ml-auto">
+                <b-nav-item-dropdown text="Opportunities" right>
+                  <b-dropdown-item href="#">Division name</b-dropdown-item>
+                  <b-dropdown-item href="#">Division name</b-dropdown-item>
+                  <b-dropdown-item href="#">Division name</b-dropdown-item>
+                  <b-dropdown-item href="#">Division name</b-dropdown-item>
+                </b-nav-item-dropdown>
+  
+                <b-nav-item-dropdown right>
+                  <template v-slot:button-content>
+                    <em>User</em>
+                  </template>
+                  <b-dropdown-item href="#">Sign in</b-dropdown-item>
+                </b-nav-item-dropdown>
+              </b-navbar-nav>
+          </b-collapse>
+        </b-container>
+      </b-navbar>
     </div>
-    <router-view/>
+
+    <router-view />
+    
   </div>
 </template>
 
