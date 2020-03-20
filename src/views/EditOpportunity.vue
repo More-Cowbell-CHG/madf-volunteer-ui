afe8ef9688cba721e55849c96d64997a13dfc659
Brian Barrow <brian.barrow@chghealthcare.com>
2020-03-20 10:54:17 -0600

    cleaned up opportunity view, added edit route and component

---
+++ src/views/EditOpportunity.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="edit-opportunity-view">
+    <b-spinner v-if="!oppData" style="width: 3rem; height: 3rem;" label="Large Spinner"></b-spinner>
+
+    <b-button type="button" @click="handleCancel" variant="primary">Cancel</b-button>
+
+    <OpportunityForm :editMode="true" :editFormData="oppData" />
+  </div>
+</template>
+
+<script>
+import opportunityList from "@/assets/opportunities.json";
+import OpportunityForm from "@/components/OpportunityForm";
+
+export default {
+  components: {
+    OpportunityForm
+  },
+  data: function() {
+    return {
+      oppData: opportunityList.find(x => x._id === this.$route.params.id)
+    };
+  },
+  computed: {},
+  methods: {
+    handleCancel() {
+      this.$router.go(-1);
+    }
+  },
+  mounted: function() {}
+};
+</script>
+
+<style lang="scss">
+</style>
--- src/views/Opportunity.vue
+++ src/views/Opportunity.vue
@@ -1,12 +1,45 @@
 <template>
-  <OpportunityFullView />
+  <div id="opportunity-view">
+    <b-spinner v-if="!oppData" style="width: 3rem; height: 3rem;" label="Large Spinner"></b-spinner>
+    <template v-else>
+      <b-button v-if="isChampion" type="button" @click="handleEditMode" variant="primary">Edit</b-button>
+      <b-button v-if="isAdmin" type="button" @click="handleDelete" variant="danger">Delete</b-button>
+
+      <OpportunityFullView :oppData="oppData" />
+    </template>
+  </div>
 </template>
 
 <script>
+import opportunityList from "@/assets/opportunities.json";
 import OpportunityFullView from "@/components/OpportunityFullView";
+
 export default {
   components: {
     OpportunityFullView
+  },
+  data: function() {
+    return {
+      oppData: opportunityList.find(x => x._id === this.$route.params.id)
+    };
+  },
+  computed: {
+    isChampion: function() {
+      // This will have logic checking the user info that comes back from DB
+      return true;
+    },
+    isAdmin: function() {
+      // This will have logic checking the user info that comes back from DB
+      return true;
+    }
+  },
+  methods: {
+    handleEditMode() {
+      this.$router.push(`/edit/opportunity/${this.oppData._id}`);
+    },
+    handleDelete() {
+      // Delete call to db via axios
+    }
   }
 };
 </script>
