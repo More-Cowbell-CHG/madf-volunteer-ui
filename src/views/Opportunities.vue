06c8741a5952914ccb2a2d0f5902e0845f4b3d7b
Chris Dixon <chris.dixon@chghealthcare.com>
2020-03-20 09:55:46 -0600

    Added the ability to filter opportunities by Status if user is an admin

--- src/views/Opportunities.vue
+++ src/views/Opportunities.vue
@@ -2,12 +2,21 @@
   <div class="opportunities">
     <h1>List of Opportunities</h1>
     <BContainer>
-      <b-form-group label="Using options array:">
+      <b-button v-if="isAdmin" v-on:click="filterByStatus = false" class="filterButton">Office</b-button>
+      <b-button v-if="isAdmin" v-on:click="filterByStatus = true" class="filterButton">Status</b-button>
+      <b-form-group :label="`Filter By ${filterByStatus ? 'Status' : 'Office'}:`">
         <b-form-checkbox-group
           id="checkbox-group-1"
           v-model="selectedOffices"
           :options="options"
           name="offices-1"
+        ></b-form-checkbox-group>
+        <b-form-checkbox-group
+          v-if="isAdmin"
+          id="checkbox-group-2"
+          v-model="selectedStatuses"
+          :options="statusOptions"
+          name="statuses-1"
         ></b-form-checkbox-group>
       </b-form-group>
       <BListGroup>
@@ -23,24 +32,43 @@ import opportunityList from "@/assets/opportunitiesGet.json";
 export default {
   data: function() {
     return {
+      isAdmin: true,
+      filterByStatus: false,
       selectedOffices: ["SLC", "FLD", "RNC", "MIC"], // Must be an array reference!
       options: [
         { text: "Salt Lake City", value: "SLC" },
         { text: "Ft. Lauderdale", value: "FLD" },
         { text: "Raleigh", value: "RNC" },
         { text: "Michigan", value: "MIC" }
+      ],
+      selectedStatuses: ["pending", "open", "closed", "archived"],
+      statusOptions: [
+        { text: "pending", value: "pending" },
+        { text: "open", value: "open" },
+        { text: "closed", value: "closed" },
+        { text: "archived", value: "archived" }
       ]
     };
   },
   computed: {
     opportunities: function() {
-      return opportunityList.opportunities.filter(opp => {
-        for (let i = 0; i < this.selectedOffices.length; i++) {
-          if (opp.office === this.selectedOffices[i]) {
-            return true;
+      if (this.filterByStatus) {
+        return opportunityList.opportunities.filter(opp => {
+          for (let i = 0; i < this.selectedStatuses.length; i++) {
+            if (opp.status === this.selectedStatuses[i]) {
+              return true;
+            }
           }
-        }
-      });
+        });
+      } else {
+        return opportunityList.opportunities.filter(opp => {
+          for (let i = 0; i < this.selectedOffices.length; i++) {
+            if (opp.office === this.selectedOffices[i]) {
+              return true;
+            }
+          }
+        });
+      }
     }
   },
   components: {
@@ -50,4 +78,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.filterButton {
+  margin: 3px;
+}
 </style>
